69addcf126d1a1d468c600447992ef6b52f260fb
Vishal Prajapati <prajapativicky678@gmail.com>
2022-03-13 19:28:06 +0530

    Day1

---
+++ Day2/script.js
@@ -0,0 +1,53 @@
+//const form = document.querySelector('form')
+//
+//form.addEventListener('submit', (event) => {
+//    event.preventDefault()
+//
+//    const name = document.querySelector('#name')
+//    const address = document.querySelector('#address')
+//
+//    const nameValue = name.value
+//    const addressValue = address.value
+//    
+//    console.log(nameValue)
+//    console.log(addressValue)
+//    if (!nameValue) {
+//        alert("Eneter name")
+//        return
+//    }
+//
+//    if (!addressValue) {
+//        alert("Eneter Address")
+//        return
+//    }
+//})
+
+const input = document.querySelector("#title")
+const errorMessage = document.querySelector("#error")
+console.log(input);
+
+function validateName(nameValue) {
+    if (!nameValue) {
+        return "name is required.."
+    }
+
+    if (nameValue.length < 5) {
+        return "name must be more then 5 char"
+    }
+
+    return null
+}
+
+input.addEventListener("keyup", (event) => {
+    console.log(input.value)
+    const value = input.value
+
+    const error = validateName(value.trim())
+    if (error) {
+        errorMessage.style.display = 'block'
+        errorMessage.innerHTML = error
+    } else {
+        errorMessage.style.display = 'none'
+    }
+
+})
